ab5f7ca5c3a91e9fb3fd4ba4933ea61ab3fc197b
OPI-py <oleksandrp09@gmail.com>
2020-12-04 14:24:16 +0200

    Tests and modifications

--- basic/snippets/models.py
+++ basic/snippets/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
+#from pygments.lexers import get_all_lexers
+#from pygments.styles import get_all_styles
 
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+#LEXERS = [item for item in get_all_lexers() if item[1]]
+#LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+#STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+LANGUAGE_CHOICES = (("e", "English"), ("u", "Ukrainian"), ("p", "Polish"))
+STYLE_CHOICES = (("f", "Formal"), ("i", "Informal"))
 
 
 class Snippet(models.Model):
@@ -12,9 +14,9 @@ class Snippet(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
-    language = models.CharField(choices=LANGUAGE_CHOICES, default='English', 
+    language = models.CharField(choices=LANGUAGE_CHOICES, default="English",
     	max_length=100)
-    style = models.CharField(choices=STYLE_CHOICES, default='Good', 
+    style = models.CharField(choices=STYLE_CHOICES, default="Formal",
     	max_length=100)
 
     class Meta:
--- basic/snippets/serializers.py
+++ basic/snippets/serializers.py
@@ -2,11 +2,20 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
-        
+#class SnippetSerializer(serializers.ModelSerializer):
+#    class Meta:
+#        model = Snippet
+#        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, 
+        max_length=100)
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, 
+        default='English')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='e')
+
     def create(self, validated_data):
         return Snippet.objects.create(**validated_data)
 
