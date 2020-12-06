99d52122543c5ad25ade371be86c483e5e511752
OPI-py <oleksandrp09@gmail.com>
2020-12-06 14:04:59 +0200

    Refactoring to ViewSets

--- basic/snippets/views.py
+++ basic/snippets/views.py
@@ -1,6 +1,6 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, viewsets
 from django.contrib.auth.models import User
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
@@ -8,28 +8,17 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
     	serializer.save(owner=self.request.user)
 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-    	IsOwnerOrReadOnly]
-
-
-class UserList(generics.ListAPIView):
-	queryset = User.objects.all()
-	serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
