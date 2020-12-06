f07d69f8a4d639f612cc90d9a8b9ae8b4a6842a5
OPI-py <oleksandrp09@gmail.com>
2020-12-06 14:10:03 +0200

    Remove redundant code

--- basic/snippets/serializers.py
+++ basic/snippets/serializers.py
@@ -11,19 +11,6 @@ class SnippetSerializer(serializers.ModelSerializer):
         fields = ['url', 'id', 'title', 'code',
             'linenos', 'language', 'style', 'owner']
 
-    def create(self, validated_data):
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-
-        return instance
-
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     snippets = serializers.HyperlinkedRelatedField(many=True,
