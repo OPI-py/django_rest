8ad93046bffebcc9fae03bd90bde035e63e77568
OPI-py <oleksandrp09@gmail.com>
2020-12-07 14:11:35 +0200

    Add admin url

--- basic/basic/urls.py
+++ basic/basic/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
 	path('', include('snippets.urls')),
 	path('api-auth/', include('rest_framework.urls')),
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
