26986c4c1cff5063e0b573710df3a80f726172e2
OPI-py <oleksandrp09@gmail.com>
2020-12-07 19:36:08 +0200

    Customize better snippet display

--- basic/snippets/admin.py
+++ basic/snippets/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from snippets.models import Snippet
 
-admin.site.register(Snippet)
+# admin.site.register(Snippet)
+
+
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+	list_display = ("title", "owner")
