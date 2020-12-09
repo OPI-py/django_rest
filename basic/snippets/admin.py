2c1b502af0d56cd1a6489b633151090a00dc83ac
OPI-py <oleksandrp09@gmail.com>
2020-12-09 13:48:54 +0200

    Remove extra field

--- basic/snippets/admin.py
+++ basic/snippets/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from snippets.models import Snippet
 
-# admin.site.register(Snippet)
-
 
 @admin.register(Snippet)
 class SnippetAdmin(admin.ModelAdmin):
--- basic/snippets/models.py
+++ basic/snippets/models.py
@@ -19,18 +19,14 @@ class Snippet(models.Model):
     	max_length=100)
     owner = models.ForeignKey('auth.User', related_name='snippets',
     	on_delete=models.CASCADE)
-    highlighted = models.TextField()
+
 
     class Meta:
         ordering = ['created']
 
     def save(self, *args, **kwargs):
-    	# lexers = get_lexer_by_name(self.language)
     	linenos = 'table' if self.linenos else False
     	options = {'title': self.title} if self.title else {}
-    	# formatter = HtmlFormatter(style=self.style, linenos=linenos,
-    		# full=True, **options)
-    	# self.highlighted = highlight(self.code, lexer, formatter)
     	super(Snippet, self).save(*args, **kwargs)
 
     def __str__(self):
